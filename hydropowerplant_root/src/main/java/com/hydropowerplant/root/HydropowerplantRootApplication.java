133942995469dbbb8d74dd7f9dfb063892f9fe60
GitMarino <marinoc1323@gmail.com>
2024-09-28 23:37:16 +0200

    Published root on GitHub

---
+++ hydropowerplant_root/src/main/java/com/hydropowerplant/root/HydropowerplantRootApplication.java
@@ -0,0 +1,13 @@
+package com.hydropowerplant.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydropowerplantRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydropowerplantRootApplication.class, args);
+	}
+
+}
---
+++ hydropowerplant_root/src/test/java/com/hydropowerplant/root/HydropowerplantRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydropowerplant.root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydropowerplantRootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
