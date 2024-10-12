376c09d889de2c7a4b9f05ecb6b0c8adceb9a841
GitMarino <marinoc1323@gmail.com>
2024-10-12 19:32:41 +0200

    HydroPowerPlant server up

--- hydropowerplant_app/src/main/java/com/hydropowerplant/app/HydroPowerPlantAppConfig.java
+++ hydropowerplant_app/src/main/java/com/hydropowerplant/app/HydroPowerPlantAppConfig.java
@@ -1,9 +1,20 @@
 package com.hydropowerplant.app;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EntityScan("com.hydropowerplant.water-level.entity")
+@EnableJpaRepositories("com.hydropowerplant.water-level.repository")
+@Configuration
+@ComponentScan(basePackages = "com.hydropowerplant.water-level.manager")
 @PropertySources({@PropertySource("classpath:database.properties"), @PropertySource("classpath:server.properties")})
+/*@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableBatchProcessing*/
 public class HydroPowerPlantAppConfig {
 
 }
--- hydropowerplant_app/src/main/java/com/hydropowerplant/app/HydroPowerPlantApplication.java
+++ hydropowerplant_app/src/main/java/com/hydropowerplant/app/HydroPowerPlantApplication.java
@@ -2,12 +2,19 @@ package com.hydropowerplant.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HydroPowerPlantApplication {
+public class HydroPowerPlantApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HydroPowerPlantApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HydroPowerPlantAppConfig.class);
 	}
 
 }
