78a50fdf4d813fcb32c4be3e494c34d934c59102
GitMarino <marinoc1323@gmail.com>
2024-11-07 22:00:31 +0100

    Device PowerLevelAction many to many

---
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/database/key/DevicePowerLevelActionRelationshipKey.java
@@ -0,0 +1,58 @@
+package com.hydropowerplant.waterlevel.database.key;
+
+
+import com.hydropowerplant.waterlevel.database.entity.Device;
+import com.hydropowerplant.waterlevel.database.entity.action.PowerLevelAction;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class DevicePowerLevelActionRelationshipKey {
+
+    @ManyToOne
+    @JoinColumn(name = "device_serial", nullable = false)
+    Device device;
+
+    @ManyToOne
+    @JoinColumn(name = "powerlevelaction_id", nullable = false)
+    PowerLevelAction powerLevelAction;
+
+    public Device getDevice() {
+        return device;
+    }
+
+    public PowerLevelAction getPowerLevelAction() {
+        return powerLevelAction;
+    }
+
+    public void setDevice(Device device) {
+        this.device = device;
+    }
+
+    public void setPowerLevelAction(PowerLevelAction powerLevelAction) {
+        this.powerLevelAction = powerLevelAction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DevicePowerLevelActionRelationshipKey that)) return false;
+        return Objects.equals(device, that.device) && Objects.equals(powerLevelAction, that.powerLevelAction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(device, powerLevelAction);
+    }
+
+    @Override
+    public String toString() {
+        return "DevicePowerLevelActionRelationshipKey{" +
+                "device=" + device +
+                ", powerLevelAction=" + powerLevelAction +
+                '}';
+    }
+}
---
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/database/relationship/DevicePowerLevelActionRelationship.java
@@ -0,0 +1,25 @@
+package com.hydropowerplant.waterlevel.database.relationship;
+
+
+import com.hydropowerplant.waterlevel.database.key.DevicePowerLevelActionRelationshipKey;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = DevicePowerLevelActionRelationship.TABLE_NAME)
+public class DevicePowerLevelActionRelationship {
+
+    public static final String TABLE_NAME = "device_power_level_action";
+
+    @EmbeddedId
+    DevicePowerLevelActionRelationshipKey id;
+
+    public DevicePowerLevelActionRelationshipKey getId() {
+        return id;
+    }
+
+    public void setId(DevicePowerLevelActionRelationshipKey id) {
+        this.id = id;
+    }
+}
