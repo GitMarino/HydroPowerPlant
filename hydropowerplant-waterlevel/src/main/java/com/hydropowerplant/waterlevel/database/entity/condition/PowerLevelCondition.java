b49bdddd6195a62f4eb624040bf0d101ec3bd403
GitMarino <marinoc1323@gmail.com>
2024-11-07 22:03:38 +0100

    Device PowerLevelCondition many to many

--- hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/database/entity/condition/PowerLevelCondition.java
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/database/entity/condition/PowerLevelCondition.java
@@ -1,11 +1,9 @@
 package com.hydropowerplant.waterlevel.database.entity.condition;
 
-import com.hydropowerplant.waterlevel.database.entity.Device;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -27,46 +25,24 @@ public class PowerLevelCondition {
     @JoinColumn(name = "condition_id")
     private Condition condition;
 
-    @ManyToOne
-    private Device device;
-
-    @Column(name = "powerlevel", nullable = false)
-    private Integer powerLevel;
-
     public Integer getId() {
         return id;
-    }
-
-    public Device getDevice() {
-        return device;
-    }
-
-    public Integer getPowerLevel() {
-        return powerLevel;
     }
 
     public void setId(Integer id) {
         this.id = id;
     }
 
-    public void setDevice(Device device) {
-        this.device = device;
-    }
-
-    public void setPowerLevel(Integer powerLevel) {
-        this.powerLevel = powerLevel;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof PowerLevelCondition that)) return false;
-        return Objects.equals(id, that.id) && Objects.equals(condition, that.condition) && Objects.equals(device, that.device) && Objects.equals(powerLevel, that.powerLevel);
+        return Objects.equals(id, that.id) && Objects.equals(condition, that.condition);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, condition, device, powerLevel);
+        return Objects.hash(id, condition);
     }
 
     @Override
@@ -74,8 +50,6 @@ public class PowerLevelCondition {
         return "PowerLevelCondition{" +
                 "id=" + id +
                 ", condition=" + condition +
-                ", device=" + device +
-                ", powerLevel=" + powerLevel +
                 '}';
     }
 }
