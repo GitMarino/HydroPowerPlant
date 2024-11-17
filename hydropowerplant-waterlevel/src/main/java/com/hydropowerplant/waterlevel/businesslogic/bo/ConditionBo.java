899827c791443fa37e33b9a2fcad515cd7678c2c
GitMarino <marinoc1323@gmail.com>
2024-11-17 02:38:13 +0100

    Javadoc

--- hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/businesslogic/bo/ConditionBo.java
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/businesslogic/bo/ConditionBo.java
@@ -1,14 +1,14 @@
 package com.hydropowerplant.waterlevel.businesslogic.bo;
 
-import com.hydropowerplant.waterlevel.ws.dto.DeviceStatus;
+import com.hydropowerplant.waterlevel.ws.dto.DeviceLogDto;
 
 public interface ConditionBo {
 
     /**
      * finds conditions connected to given device and sends them to scenario manager
      * <p>
-     * persists device status on database
+     * persists device power level and log on database
      */
-    void manageDevicePowerLevelCondition(DeviceStatus deviceStatus);
+    void manageDevicePowerLevelCondition(DeviceLogDto deviceLogDto);
 
 }
