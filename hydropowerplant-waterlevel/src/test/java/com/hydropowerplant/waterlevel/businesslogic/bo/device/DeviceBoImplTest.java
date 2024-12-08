8a5459a05f8966f6af4c5d9112019773e2b76eeb
GitMarino <marinoc1323@gmail.com>
2024-12-08 21:29:33 +0100

    JUnit test

---
+++ hydropowerplant-waterlevel/src/test/java/com/hydropowerplant/waterlevel/businesslogic/bo/device/DeviceBoImplTest.java
@@ -0,0 +1,37 @@
+package com.hydropowerplant.waterlevel.businesslogic.bo.device;
+
+import com.hydropowerplant.waterlevel.businesslogic.exception.ItemNotFoundException;
+import com.hydropowerplant.waterlevel.dao.device.DeviceDao;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DeviceBoImplTest {
+
+    @InjectMocks
+    private DeviceBoImpl deviceBoImpl;
+
+    @Mock
+    private DeviceDao deviceDao;
+
+    @Test
+    public void setPowerLevelThrowsItemNotFoundExceptionTest() {
+        // Arrange
+        String serial = "cda899e-fb9c-47ad-9866";
+        double powerLevel = 50.0;
+        when(deviceDao.findById(serial)).thenReturn(Optional.empty());
+
+        // Act & Assert
+        assertThrows(ItemNotFoundException.class, () -> deviceBoImpl.setPowerLevel(serial, powerLevel));
+    }
+
+
+}
