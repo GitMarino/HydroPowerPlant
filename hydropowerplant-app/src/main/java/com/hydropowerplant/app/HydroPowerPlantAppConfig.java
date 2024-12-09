19e31dd54988136c06cd5b3370c6a015d07c3f1e
GitMarino <marinoc1323@gmail.com>
2024-12-09 18:16:26 +0100

    Application start

--- hydropowerplant-app/src/main/java/com/hydropowerplant/app/HydroPowerPlantAppConfig.java
+++ hydropowerplant-app/src/main/java/com/hydropowerplant/app/HydroPowerPlantAppConfig.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EntityScan("com.hydropowerplant.waterlevel.entity")
-@EnableJpaRepositories("com.hydropowerplant.waterlevel.dao")
-@ComponentScan(basePackages = "com.hydropowerplant.waterlevel.bo")
+@EnableJpaRepositories("com.hydropowerplant.waterlevel.repository")
+@ComponentScan(basePackages = "com.hydropowerplant.waterlevel.businesslogic.service")
 @PropertySources({
         @PropertySource("classpath:database.properties"),
         @PropertySource("classpath:mail.properties"),
