35f643c86561a72722d04c55f022c937364c3bab
GitMarino <marinoc1323@gmail.com>
2024-12-14 17:15:50 +0100

    No relationship entities

--- hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/repository/condition/PowerLevelConditionDao.java
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/repository/condition/PowerLevelConditionDao.java
@@ -2,16 +2,8 @@ package com.hydropowerplant.waterlevel.repository.condition;
 
 import com.hydropowerplant.waterlevel.entity.condition.PowerLevelCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface PowerLevelConditionDao extends JpaRepository<PowerLevelCondition, Integer> {
-    
-    @Query("SELECT id.powerLevelCondition.id "
-            + "FROM DevicePowerLevelConditionRelationship "
-            + "WHERE id.device.serial=?1")
-    List<Integer> findByDeviceSerial(String deviceSerial);
 }
