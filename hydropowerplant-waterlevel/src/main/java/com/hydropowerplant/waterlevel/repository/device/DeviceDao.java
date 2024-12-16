9f4a3c70eb9089f9b9cd6340eab6b8dcf7c84123
GitMarino <marinoc1323@gmail.com>
2024-12-16 21:17:22 +0100

    @Modifing and update needs @Transactional

--- hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/repository/device/DeviceDao.java
+++ hydropowerplant-waterlevel/src/main/java/com/hydropowerplant/waterlevel/repository/device/DeviceDao.java
@@ -1,6 +1,7 @@
 package com.hydropowerplant.waterlevel.repository.device;
 
 import com.hydropowerplant.waterlevel.entity.device.Device;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface DeviceDao extends JpaRepository<Device, String> {
 
     @Modifying
+    @Transactional
     @Query("UPDATE Device SET powerLevel = :powerLevel WHERE serial = :serial")
     int updatePowerLevelBySerial(@Param("serial") String serial, @Param("powerLevel") double powerLevel);
     
